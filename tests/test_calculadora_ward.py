6e330eaea278e05ff0a26d6c55f5413c574ee85c
mauricio-sousa <mauricio5891@gmail.com>
2023-04-25 19:04:17 -0300

    Add test with ward

--- tests/test_calculadora_ward.py
+++ tests/test_calculadora_ward.py
@@ -1,30 +1,31 @@
-import pytest
 from calculadora.calc import soma, subtrai, divide, multiplica
 from faker import Faker
+from ward import raises, test, skip
+
 
 faker = Faker()
 x = faker.random_digit()
 y = faker.random_digit()
 
 
+@test("simple addition")
 def test_soma():
     assert soma(x, y) == x + y
 
-
+@test("simple subtraction")
 def test_subtrai():
     assert subtrai(x, y) == x - y
 
-
-@pytest.mark.skipif(y == 0, reason="Zero Division")
+@test("Skipped Zero Division")
+@skip("Skipped Zero Division", when=y == 0)
 def test_divide():
     assert divide(x, y) == x / y
 
-
-def test_multiplica():
+@test("simple multiplication")
+def test_multiplicacao():
     assert multiplica(x, y) == x * y
-
-
+ 
+@test("a ZeroDivision error is raised when we divide by 0")
 def test_zero_division():
-    with pytest.raises(ValueError) as err:
-        divide(1, 0)
-    assert str(err.value) == 'Cannot divide by Zero'
+    with raises(ZeroDivisionError):
+        1 / 0    
