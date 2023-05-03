b811aeffecbbb8c44d1e56b9ce161ed06ca1719d
mauricio-sousa <mauricio5891@gmail.com>
2023-05-02 22:51:05 -0300

    Add default decorator @ward.test

--- tests/test_calculadora_ward.py
+++ tests/test_calculadora_ward.py
@@ -1,6 +1,7 @@
 from calculadora.calc import soma, subtrai, divide, multiplica
 from faker import Faker
-from ward import raises, test, skip
+from ward import raises, skip
+import ward
 
 
 faker = Faker()
@@ -8,28 +9,28 @@ x = faker.random_digit()
 y = faker.random_digit()
 
 
-@test("simple addition")
+@ward.test("simple addition")
 def test_soma():
     assert soma(x, y) == x + y
 
 
-@test("simple subtraction")
+@ward.test("simple subtraction")
 def test_subtrai():
     assert subtrai(x, y) == x - y
 
 
-@test("Skipped Zero Division")
+@ward.test("Skipped Zero Division")
 @skip("Skipped Zero Division", when=y == 0)
 def test_divide():
     assert divide(x, y) == x / y
 
 
-@test("simple multiplication")
+@ward.test("simple multiplication")
 def test_multiplicacao():
     assert multiplica(x, y) == x * y
 
 
-@test("a ZeroDivision error is raised when we divide by 0")
+@ward.test("a ZeroDivision error is raised when we divide by 0")
 def test_zero_division():
     with raises(ZeroDivisionError):
         1 / 0
